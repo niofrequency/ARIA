a84ccc89461aa40585013490d35cad31bf4da1e5
niofrequency <124836368+niofrequency@users.noreply.github.com>
2026-02-17 15:19:24 +0900

    Update generateAriaImage.ts

--- services/generateAriaImage.ts
+++ services/generateAriaImage.ts
@@ -12,6 +12,7 @@ const LORA_MAP: Record<string, string> = {
   "brittany": "brittanyspears-v1",
   "lia-marie": "lia-marie",
   "naveen": "naveen",
+  "velma-dinkle": "velma-dinkle",
   "regina-evilqueen":"regina-evilqueen",
   "maria-brazil":"maria-brazil",
   "larkin-love": "larkin-love",
