7c5f1b8078c68a775705754dc8e2ec3811056fb3
niofrequency <124836368+niofrequency@users.noreply.github.com>
2026-05-30 01:09:44 +0900

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -153,7 +153,7 @@ return (
               <img src="/img/ARIA-LOGO.PNG" alt="Logo" className="relative w-10 h-10 object-contain" />
             </div>
             <div>
-              <span className="text-xl font-light text-white tracking-widest uppercase">ARIA </span>
+              <span className="text-xl font-light tracking-[0.5em] text-white tracking-widest uppercase">ARIA </span>
             </div>
           </div>
           
