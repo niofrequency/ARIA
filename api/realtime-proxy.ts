c9acc649cb6553629da585df3cb1b802e71f64a1
niofrequency <124836368+niofrequency@users.noreply.github.com>
2026-06-06 09:47:16 +0900

    Update realtime-proxy.ts

--- api/realtime-proxy.ts
+++ api/realtime-proxy.ts
@@ -1,4 +1,4 @@
-// api/realtime-proxy.ts
+// api/realtime-token.ts // FOR CALLING TO BOT
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { WebSocketServer, WebSocket } from 'ws';
 
